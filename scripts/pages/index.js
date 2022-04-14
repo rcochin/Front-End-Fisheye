e82316cb113fdc5450d1202ec2d0232004c9fbee
rcochin <romain.cochin.01@gmail.com>
2022-04-14 11:13:01 +0200

    test

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,5 +1,5 @@
     async function getPhotographers() {
-        fetch('../../data/photographers.json')
+        fetch('./data/photographers.json')
         .then(response => {
             return response.json();
           }).then(data => {
