b92fd981985b31cb4352e1926a96a4d8c7d0e9bf
rcochin <romain.cochin.01@gmail.com>
2022-04-25 18:22:47 +0200

    navigation tabulation

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -19,29 +19,59 @@ function mediaFactory(data, photographername, count) {
         coeur.textContent = likes;
         heart.setAttribute('class', 'fas fa-heart');
         heart.setAttribute('aria-hidden', 'true');
+        heart.setAttribute('tabindex', '0');
         heartDIV.setAttribute("class", "heart-container");
         desc.setAttribute("class", "desc-media");
         imageDIV.setAttribute("class", "bloc-media");
+        heart.addEventListener("keypress", function(event) {
+            // If the user presses the "Enter" key on the keyboard
+            if (event.key === "Enter") {
+              // Cancel the default action, if needed
+              event.preventDefault();
+              // Trigger the button element with a click
+              heart.click();
+            }
+          });
         if(video){
             mp4.setAttribute("class", "imgMedia");
             mp4.setAttribute("id", "video");
-            mp4.setAttribute("data-id", count);
+            mp4.setAttribute("data-id", "1");
             mp4.setAttribute("src", videoMP4);
             mp4.setAttribute("type", "video/mp4");
+            mp4.setAttribute("tabindex", "0");
             mp4.autoplay = true;
             mp4.loop = true;
             imageDIV.append(mp4, desc);
+            mp4.addEventListener("keypress", function(event) {
+                // If the user presses the "Enter" key on the keyboard
+                if (event.key === "Enter") {
+                  // Cancel the default action, if needed
+                  event.preventDefault();
+                  // Trigger the button element with a click
+                  mp4.click();
+                }
+              });
         }else{
             img.setAttribute("class", "imgMedia");
             img.setAttribute("data-id", count);
             img.setAttribute("alt", title);
             img.setAttribute("src", picture);
+            img.setAttribute("tabindex", "0");
             imageDIV.append(img, desc);
+            img.addEventListener("keypress", function(event) {
+                // If the user presses the "Enter" key on the keyboard
+                if (event.key === "Enter") {
+                  // Cancel the default action, if needed
+                  event.preventDefault();
+                  // Trigger the button element with a click
+                  img.click();
+                }
+              });
         }
+        
         heartDIV.append(coeur, heart);
         desc.append(h3, heartDIV);
         return (imageDIV)
     }
     return { getUserCardDOM }
-} 
- 
+}
